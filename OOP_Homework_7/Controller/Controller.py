0bf3224cc7f3d4fb033a3ee27e6c8c52ddf10b40
Alina <gav621@mail.ru>
2023-04-23 18:27:29 +0300

    oop homework 7

---
+++ OOP_Homework_7/Controller/Controller.py
@@ -0,0 +1,34 @@
+from OOP_Homework_7.Model.Products.BottleOfWater import BottleOfWater
+from OOP_Homework_7.Model.Products.HotDrinks import HotDrinks
+from OOP_Homework_7.Model.VendingMachine import VendingMachine
+from OOP_Homework_7.View.ConsoleInterface import ConsoleInterface
+
+
+class Controller:
+
+    def start(self):
+        console = ConsoleInterface()
+        list_hot_drinks = [HotDrinks('Milk', 2, 0.3, 80),
+                           HotDrinks('Tea', 1, 0.4, 70),
+                           HotDrinks('Coffee', 3, 1.0, 65)]
+        list_bottle_of_water = [BottleOfWater('Fanta', 3, 0.5),
+                                BottleOfWater('Sprite', 3.5, 0.45),
+                                BottleOfWater('Cola', 3.2, 0.43)]
+        vending_machine1 = VendingMachine()
+        vending_machine2 = VendingMachine()
+        console.print_message('-----Список горячих напитков добавлен в торговый автомат-----')
+        vending_machine1.list_products = list_hot_drinks
+        console.print_list_product(vending_machine1.list_products)
+        console.print_message('-----Список бутилированных напитков добавлен в торговый автомат-----')
+        vending_machine2.list_products = list_bottle_of_water
+        console.print_list_product(vending_machine2.list_products)
+        console.print_message('-----В горячие напитки добавлено какао-----')
+        vending_machine1.add_at_list(HotDrinks('Какао', 2.5, 0.5, 70))
+        console.print_list_product(vending_machine1.list_products)
+        console.print_message('-----В бутилированные напитки добавлена минералка-----')
+        vending_machine2.add_at_list(BottleOfWater('Минералка', 1.5, 0.5))
+        console.print_list_product(vending_machine2.list_products)
+        console.print_message('-----Получение Tea из торгового автомата-----')
+        console.print_message(vending_machine1.get_product('Tea'))
+        console.print_message('-----Получение Mirinda из торгового автомата-----')
+        console.print_message(vending_machine2.get_product('Mirinda'))
---
+++ OOP_Homework_7/View/ConsoleInterface.py
@@ -0,0 +1,8 @@
+class ConsoleInterface:
+
+    def print_list_product(self, list_products: list):
+        for el in list_products:
+            print(el)
+
+    def print_message(self, text):
+        print(text)
